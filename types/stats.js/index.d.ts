61e6157e8a5f84c715a72f7227e2be170016926d
Guilherme Pereira <guilospanck@protonmail.com>
2025-01-11 16:13:05 +0000

    Add getFPS function

---
+++ types/stats.js/index.d.ts
@@ -0,0 +1,28 @@
+declare class Stats {
+  constructor();
+  REVISION: number;
+  dom: HTMLDivElement;
+
+  /**
+   * @param value 0:fps, 1: ms, 2: mb, 3+: custom
+   */
+  showPanel(value: number): void;
+  begin(): void;
+  end(): number;
+  update(): void;
+  getFPS(): number;
+
+  addPanel(panel: Stats.Panel): Stats.Panel;
+}
+
+declare namespace Stats {
+  class Panel {
+    constructor(name: string, foregroundColor: string, backgroundColor: string);
+    dom: HTMLCanvasElement;
+    update(value: number, maxValue: number): void;
+  }
+}
+
+declare module "stats.js" {
+  export = Stats;
+}
